1a926f17f43147e492734f6a3819bd9b198a82f8
bombbomb <kazokuninarouyo1225@gmail.com>
2026-05-07 16:26:57 +0900

    課題3提出

--- 04_simd/11_nbody.cpp
+++ 04_simd/11_nbody.cpp
@@ -1,6 +1,6 @@
 #include <cstdio>
 #include <cstdlib>
-#include <cmath>
+#include <x86intrin.h>
 
 int main() {
   const int N = 16;
@@ -11,16 +11,26 @@ int main() {
     m[i] = drand48();
     fx[i] = fy[i] = 0;
   }
+
+  __m512 xj = _mm512_loadu_ps(x);
+  __m512 yj = _mm512_loadu_ps(y);
+  __m512 mj = _mm512_loadu_ps(m);
+
   for(int i=0; i<N; i++) {
-    for(int j=0; j<N; j++) {
-      if(i != j) {
-        float rx = x[i] - x[j];
-        float ry = y[i] - y[j];
-        float r = std::sqrt(rx * rx + ry * ry);
-        fx[i] -= rx * m[j] / (r * r * r);
-        fy[i] -= ry * m[j] / (r * r * r);
-      }
-    }
+    __m512 xi = _mm512_set1_ps(x[i]);
+    __m512 yi = _mm512_set1_ps(y[i]);
+    __m512 rx = _mm512_sub_ps(xi, xj);
+    __m512 ry = _mm512_sub_ps(yi, yj);
+    __m512 r2 = _mm512_add_ps(_mm512_mul_ps(rx, rx), _mm512_mul_ps(ry, ry));
+
+    __mmask16 mask = 0xffff ^ (1 << i);
+    r2 = _mm512_mask_blend_ps(mask, _mm512_set1_ps(1.0f), r2);
+    __m512 inv_r = _mm512_rsqrt14_ps(r2);
+    __m512 inv_r3 = _mm512_mul_ps(_mm512_mul_ps(inv_r, inv_r), inv_r);
+    __m512 fm = _mm512_mul_ps(mj, inv_r3);
+
+    fx[i] -= _mm512_reduce_add_ps(_mm512_maskz_mul_ps(mask, rx, fm));
+    fy[i] -= _mm512_reduce_add_ps(_mm512_maskz_mul_ps(mask, ry, fm));
     printf("%d %g %g\n",i,fx[i],fy[i]);
   }
 }
