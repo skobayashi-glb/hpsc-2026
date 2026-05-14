344922b7bac12d7cdf705e1525476f91c157a3b9
bombbomb <kazokuninarouyo1225@gmail.com>
2026-05-14 18:06:19 +0900

    課題4提出

--- 06_cuda/13_bucket_sort.cpp
+++ 06_cuda/13_bucket_sort.cpp
@@ -2,23 +2,31 @@
 #include <cstdlib>
 #include <vector>
 
+__global__ void count_kernel(int *key, int *bucket, int n) {
+  int i = blockIdx.x * blockDim.x + threadIdx.x;
+  if (i < n) {
+    atomicAdd(&bucket[key[i]], 1);
+  }
+}
+
 int main() {
   int n = 50;
   int range = 5;
-  std::vector<int> key(n);
+  int *key, *bucket;
+  cudaMallocManaged(&key, n * sizeof(int));
+  cudaMallocManaged(&bucket, range * sizeof(int));
+
   for (int i=0; i<n; i++) {
     key[i] = rand() % range;
     printf("%d ",key[i]);
   }
   printf("\n");
 
-  std::vector<int> bucket(range); 
-  for (int i=0; i<range; i++) {
-    bucket[i] = 0;
-  }
-  for (int i=0; i<n; i++) {
-    bucket[key[i]]++;
-  }
+  for (int i=0; i<range; i++) bucket[i] = 0;
+
+  count_kernel<<<1, n>>>(key, bucket, n);
+  cudaDeviceSynchronize();
+
   for (int i=0, j=0; i<range; i++) {
     for (; bucket[i]>0; bucket[i]--) {
       key[j++] = i;
